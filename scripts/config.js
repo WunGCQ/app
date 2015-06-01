ef35f88efcbe6d53fd12ca5167f1a78e9139ccf4
jl <i.dragonxx@gmail.com>
2015-06-01 16:49:19 +0800

    resolve abs url for apis

--- scripts/config.js
+++ scripts/config.js
@@ -1,18 +1,17 @@
-define('CONF',function(){
+define('CONF', function() {
     var CONF = {};
     var API = CONF.API = {};
     var admin = API.admin = {};
     var public = API.public = {};
 
     var apiRoot = 'http://ecpkn.com';
+    apiRoot = '';
     API.root = apiRoot;
     API.news = API.root + '/admin/news';
     API.record = API.root + '/records';
 
     admin.root = API.root + '/admin';
     admin.news = admin.root + '/news';
-
-
 
     return CONF;
 });
